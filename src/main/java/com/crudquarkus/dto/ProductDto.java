aad3e63389ca2eb748f9e73ae087620886462f8c
SilvFelipe <felipesilva29@terra.com.br>
2024-08-06 13:32:05 -0300

    Adicionado endpoint delete

--- src/main/java/com/crudquarkus/dto/ProductDto.java
+++ src/main/java/com/crudquarkus/dto/ProductDto.java
@@ -1,10 +1,19 @@
 package com.crudquarkus.dto;
 
-public class ProductDto {
+public class  ProductDto {
 
+    private Long id;
     private String nome;
     private Long stock;
     private Double preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public void setNome(String nome) {
         this.nome = nome;
